ec2f1d8012b632d44a9aa2414c8387d70a2e23f6
friend <10142175+friend-team@user.noreply.gitee.com>
2026-04-22 15:09:50 +0800

    modified:   src/Config/rabbitmq.php

--- src/Config/rabbitmq.php
+++ src/Config/rabbitmq.php
@@ -6,7 +6,7 @@ return [
     // RabbitMQ 连接配置
     'connection' => [
         'host'     => env('RABBITMQ_HOST', '127.0.0.1'),
-        'port'     => (int) env('RABBITMQ_PORT', 5672),
+        'port'     => (int) env('RABBITMQ_PORT', '5672'),
         'user'     => env('RABBITMQ_USER', 'guest'),
         'password' => env('RABBITMQ_PASSWORD', 'guest'),
         'vhost'    => env('RABBITMQ_VHOST', '/'),
